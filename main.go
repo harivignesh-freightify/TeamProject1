13254cd017dfe6b29898cae51bafa902739f9069
HariVignesh18 <harivirat2005@gmail.com>
2025-10-15 12:03:30 +0530

    First Commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// Parse all templates
+var tmpl = template.Must(template.ParseGlob("templates/*.html"))
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		tmpl.ExecuteTemplate(w, "index.html", map[string]string{
+			"Title": "Home Page",
+			"Body":  "Welcome to my first Go website 🚀",
+		})
+	case "/about":
+		tmpl.ExecuteTemplate(w, "about.html", map[string]string{
+			"Title": "About Page",
+			"Body":  "This is a simple website built with Go!",
+		})
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func main() {
+	// Serve static files (CSS, JS, images)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handle routes
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
